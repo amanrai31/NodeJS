e7fd3675cb39ca889932551025202186ded55a4b
amanrai31 <aman970rai@gmail.com>
2025-11-14 09:39:44 +0530

    fileSystem - write,read,append

--- 01node/file.js
+++ 01node/file.js
@@ -3,7 +3,7 @@ const fileSystem = require("fs");
 // Sync - Will write/create file synchronously
 // const file = fileSystem.writeFileSync("./sample.txt", "Write file using nodeJS fs");
 
-// Async - write file
+// Async - write file => appendFile(path, data, callback)
 fileSystem.writeFile("./sample.txt", "Write file using Async nodeJS fs", (err)=>{});
 
 // Sync - read file
@@ -19,3 +19,8 @@ fileSystem.readFile("./countries.txt", "utf-8", (err,res)=>{
         console.log(res);
     }
 })
+
+// Async append file - appendFile(path, data, callback) - We can use to append log for every req.
+fileSystem.appendFile("./countries.txt", `${Date.now()}\n`,(err)=>{
+    if(err) console.log("Error while appending", err);
+});
