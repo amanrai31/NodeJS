1a744e496e6d7a9bbe29bb81e0d25e790842cb96
amanrai31 <aman970rai@gmail.com>
2025-11-17 00:28:12 +0530

    CRUD

--- 03node/README.md
+++ 03node/README.md
@@ -3,3 +3,10 @@
 Whatever we did in 02node application using raw http module (made a simple http server) - we will do that using EXPRESS.
 
 Express internally has `http module` & manage a lot of things for us.
+
+
+## Rest APIs
+
+Works on Client-server architecture, respect http methods(best practice). We can send Text, media, JSON, HTML etc over REST
+
+**NOTE :** We used mockaroo to mock DB.
--- 03node/index.js
+++ 03node/index.js
@@ -1,7 +1,64 @@
 const express = require("express");
+const data = require("./MOCK_DATA.json");
+const fs = require("fs");
 
 const app = express();
 const port = 8000;
+app.listen(port, ()=> console.log("Server started"));
+
+// Middleware for request Body
+app.use(express.urlencoded({extended:false}));
+
+// GET at /user will return server side rendered HTML - Only browser can use (SSR)
+app.get("/users", (req,res)=>{
+  const html = `
+      <ul>
+        ${data.map((entry)=> {
+        return `<li> ${entry.first_name} </li>`
+      }).join("")}</ul>
+  `
+  res.send(html);
+})
+
+// GET  at /api/users will return users (JSON data). Any client(browser, mobile) can use accordingly 
+app.route("/api/users").get((req,res)=>{
+    res.json(data);
+}).post((req,res)=>{
+    const reqBody = req.body;
+    const id = data.length + 1;
+    const newUser = {id, ...reqBody};
+    data.push(newUser);
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(data), 
+      (err,data)=>{
+        if(err){
+        console.log("Error",err)}
+        else{
+            res.send(`User created successfully. UserID - ${id} `);
+        }
+    })
+    console.log(reqBody);
+})
+
+
+// NOTE: If we have same route & have multiple HTTP Methods - app.route("/route").get(...).post(...).delete(...)
+
+// Dynamic route - app.get("/user/:id")
+
+app.route("/api/users/:id").get((re,res)=>{
+  const id = Number(re.params.id);
+  const user = data.find((entry)=> entry.id===id);
+  return res.json(user);
+}).patch((req,res)=>{
+    // When there was nothing inside this handler - It throws a HTML with Error - Cannot PATCH /api/users
+    const reqBody = req.body;
+    console.log(reqBody);
+    
+   
+}).delete((re,res)=>{
+
+})
+
+
 
 app.get("/", (req,res)=>{
     return res.send("Home");
@@ -9,4 +66,3 @@ app.get("/", (req,res)=>{
 app.get("/about",(req,res)=>{
     return res.send("About");
 })
-app.listen(port, ()=> console.log("Server started"));
