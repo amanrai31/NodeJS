b9c6e42b8c266cb2853e823711e801a0f7c6108a
amanrai31 <aman970rai@gmail.com>
2025-12-07 22:28:53 +0530

    patch & delete in 03node

--- 03node/README.md
+++ 03node/README.md
@@ -1,5 +1,7 @@
 # Express
 
+`npm i express`
+
 Whatever we did in 02node application using raw http module (made a simple http server) - we will do that using EXPRESS.
 
 Express internally has `http module` & manage a lot of things for us.
@@ -10,3 +12,7 @@ Express internally has `http module` & manage a lot of things for us.
 Works on Client-server architecture, respect http methods(best practice). We can send Text, media, JSON, HTML etc over REST
 
 **NOTE :** We used mockaroo to mock DB.
+
+**NOTE :** We should send response as `res.json(...object)` - RECOMMENDED or we can use `res.send(...some string message)`
+
+**NOTE :** POST method - we fill req body as - `x-www-form-urlencoded` (new), 
--- 03node/index.js
+++ 03node/index.js
@@ -6,8 +6,12 @@ const app = express();
 const port = 8000;
 app.listen(port, ()=> console.log("Server started"));
 
-// Middleware for request Body
+// Middleware for request Body in POST requests
 app.use(express.urlencoded({extended:false}));
+
+app.get("/", (req,res)=>{
+  return res.send("Home => Visit /users OR /api/users");
+})
 
 // GET at /user will return server side rendered HTML - Only browser can use (SSR)
 app.get("/users", (req,res)=>{
@@ -15,29 +19,27 @@ app.get("/users", (req,res)=>{
   <h1>SERVER SIDE RENDERED HTML</h1>
       <ul>
         ${data.map((entry)=> {
-        return `<li> <p style="color:red;" > ${entry.first_name} </p> </li>`
-      }).join("")}</ul>
+        return `<li> <p style="color:red;" > ${entry.first_name} </p> </li>`})
+        .join("")}
+      </ul>
   `
   res.send(html);
 })
 
 // GET  at /api/users will return users (JSON data). Any client(browser, mobile) can use accordingly 
 app.route("/api/users").get((req,res)=>{
-    res.json(data);
+    res.send(data);   // data is already in json format so i just send otherwise we should use res.json 
 }).post((req,res)=>{
-    const reqBody = req.body;
-    const id = data.length + 1;
-    const newUser = {id, ...reqBody};
-    data.push(newUser);
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(data), 
-      (err,data)=>{
-        if(err){
-        console.log("Error",err)}
-        else{
-            res.send(`User created successfully. UserID - ${id} `);
-        }
+    const newUser = req.body;
+    data.push({id:data.length+1, ...newUser});
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(data), (err,succ)=>{
+      if(err){
+        console.log("Error", err);
+      }
+      else{
+        res.send(`User added successfully. UserId : ${data.length}`);
+      }
     })
-    console.log(reqBody);
 })
 
 
@@ -45,25 +47,50 @@ app.route("/api/users").get((req,res)=>{
 
 // Dynamic route - app.get("/user/:id")
 
-app.route("/api/users/:id").get((re,res)=>{
-  const id = Number(re.params.id);
+app.route("/api/users/:id").get((req,res)=>{
+  console.log("Qeryyyyyy", req.params);
+  const id = Number(req.params.id);
   const user = data.find((entry)=> entry.id===id);
   return res.json(user);
 }).patch((req,res)=>{
     // When there was nothing inside this handler - It throws a HTML with Error - Cannot PATCH /api/users
     const reqBody = req.body;
     console.log(reqBody);
-    
+    console.log("paramss of /api/user/:id", req.params);
+    const userId = Number(req.params.id);
+    console.log("UserrrrID", userId);
+    let updated = false;
+    data.forEach((item,index)=>{
+      if(item.id==userId){
+        data[index] = {...item, ...reqBody};
+        updated = true;
+      }
+    });
+    if(!updated){
+      res.status(404).send("User not found");
+    }
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(data,null,2), (err,ok)=>{
+      if(err){
+        res.status(500).send("Error: Failed to update file");
+      }
+      else{
+        res.send(`Patched, UserID : ${userId}`);
+      }
+    })
    
-}).delete((re,res)=>{
-
+}).delete((req,res)=>{
+    const userId = Number(req.params.id);
+    const newData = data.filter((e)=>e.id!==userId);
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(newData, null, 2), (err,ok)=>{
+      if(err){
+        res.status(500).send("Failed to upload file");
+      }
+      else{
+        res.send(`User deleted, userID:${userId}`);
+      }
+    })
 })
 
 
 
-app.get("/", (req,res)=>{
-    return res.send("Home");
-})
-app.get("/about",(req,res)=>{
-    return res.send("About");
-})
+
